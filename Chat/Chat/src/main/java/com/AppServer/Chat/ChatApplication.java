2e5fa78f6db9d9d8ec72092b7e6d9f432889a14b
Diego Mariucci <mariuccidiego@gmail.com>
2023-10-20 12:47:42 +0200

    roba

--- Chat/Chat/src/main/java/com/AppServer/Chat/ChatApplication.java
+++ Chat/Chat/src/main/java/com/AppServer/Chat/ChatApplication.java
@@ -2,12 +2,23 @@ package com.AppServer.Chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class ChatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+}
 
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @PostMapping("/risposta")
+    public String riceviParola(@RequestBody String parola) {
+        String risposta = "Hai scritto: " + parola;
+        return risposta;
+    }
 }
